8628c049361a25b1480794b30b98899d0f027744
Patrick Barroca <pbarroca@afi-sa.fr>
2017-10-26 16:00:28 +0200

    build 64bit

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 PHARO=61
 VM=vm
 
-wget -O - get.pharo.org/$PHARO+$VM | bash
+wget -O - get.pharo.org/64/$PHARO+$VM | bash
 
-./pharo Pharo.image save GOOMBAS --delete-old
-./pharo GOOMBAS.image eval "Metacello new githubUser: 'Afibre' project: 'Goombas' commitish: 'master' path: 'src'; baseline: 'Goombas'; load. Smalltalk snapshot: true andQuit: true."
+./pharo Pharo.image save Goombas --delete-old
+./pharo Goombas.image eval "Metacello new githubUser: 'Afibre' project: 'Goombas' commitish: 'master' path: 'src'; baseline: 'Goombas'; load. Smalltalk snapshot: true andQuit: true."
 
-zip -r GOOMBAS.zip GOOMBAS.image GOOMBAS.changes
+zip -r Goombas.zip Goombas.image Goombas.changes
