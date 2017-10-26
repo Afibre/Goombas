63c424058476f1f78012a983f0df0fbbac1ee2d1
Patrick Barroca <pbarroca@afi-sa.fr>
2017-10-26 15:25:28 +0200

    repo rename in build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ VM=vm
 wget -O - get.pharo.org/$PHARO+$VM | bash
 
 ./pharo Pharo.image save GOOMBAS --delete-old
-./pharo GOOMBAS.image eval "Metacello new githubUser: 'Afibre' project: 'GOOMBAS' commitish: 'master' path: 'src'; baseline: 'Goombas'; load. Smalltalk snapshot: true andQuit: true."
+./pharo GOOMBAS.image eval "Metacello new githubUser: 'Afibre' project: 'Goombas' commitish: 'master' path: 'src'; baseline: 'Goombas'; load. Smalltalk snapshot: true andQuit: true."
 
 zip -r GOOMBAS.zip GOOMBAS.image GOOMBAS.changes
